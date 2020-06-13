bd1425e5ac3baeb2c884fb72a9b066fab66ef719
elwan7dev <ahmed.elwan568@gmail.com>
2020-06-13 05:54:05 +0200

    add confirm func

--- public/js/main.js
+++ public/js/main.js
@@ -1,10 +1,20 @@
-$(function () {
+$(function() {
     "use strict";
 
-// Confirmation Message on Button
-$(".confirm").click(function () {
-    return confirm("Are You Sure?");
-});
+    // Confirmation Message on Button
+    $(".confirm").click(function() {
+        return confirm("Are You Sure?");
+    });
 
-
+    
+   /*  // dynamic active cals
+    $("#topheader .navbar-nav a").on( 'click', function() {
+        $("#topheader .navbar-nav")
+            .find("li.active")
+            .removeClass("active");
+        $(this)
+            .parent("li")
+            .addClass("active");
+    }); */
+    
 });
