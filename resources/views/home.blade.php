0459ce9b2429bde7f6c7e3a8ac8319f65fedd4c7
elwan7dev <ahmed.elwan568@gmail.com>
2020-06-13 14:53:02 +0200

    add latest-cards for dashboard

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -2,7 +2,7 @@
 
 @section('content')
 <div class="container">
-    <div class="home-stats text-center">
+    <div class="home-stats text-center mb-4">
         <h1>Dashboard</h1>
         <div class="row">
             <div class="col-md-3">
@@ -46,6 +46,79 @@
                 </div>
             </div>
         </div>
+    </div>
+
+
+    <div class="latest">
+        <div class="row">
+            <div class="col-sm-6">
+                <!-- TABLE: LATEST USERS -->
+                <div class="card" id="users-card">
+                    <div class="card-header border-transparent">
+                        <i class="fas fa-users"></i> Latest Clients
+                        <div class="card-tools">
+                            <button type="button" class="btn btn-tool" data-card-widget="collapse">
+                                <i class="fas fa-minus"></i>
+                            </button>
+                            <button type="button" class="btn btn-tool" data-card-widget="remove">
+                                <i class="fas fa-times"></i>
+                            </button>
+                        </div>
+                    </div>
+                    <div class="card-body p-0">
+                       @if (false)
+                       <table>
+                            {{-- Future work --}}
+                       </table>
+                       @else
+                        <div class='alert alert-info m-2'><b>+ </b>Future Work</div>                 
+                       @endif
+                    </div>
+                    <!-- /.card-body -->
+                               
+                    <div class="card-footer clearfix">
+                        <a href="{{ route('clients.create') }}" class="btn btn-sm btn-primary float-left"><i class="fas fa-plus mr-1"></i> New Client</a>
+                        <a href="{{ route('clients.index') }}" class="btn btn-sm btn-secondary float-right">View All Clients</a>
+                    </div>
+                    <!-- /.card-footer -->
+                </div>
+                <!-- /.card -->
+            </div>
+            <div class="col-sm-6">
+                <!--  Statistics -->
+                <div class="card" id="items-card">
+                    <div class="card-header border-transparent">
+                        <i class="fas fa-chart-pie mr-1"></i> Statistics
+
+                        <div class="card-tools">
+                            <button type="button" class="btn btn-tool" data-card-widget="collapse">
+                                <i class="fas fa-minus"></i>
+                            </button>
+                            <button type="button" class="btn btn-tool" data-card-widget="remove">
+                                <i class="fas fa-times"></i>
+                            </button>
+                        </div>
+                    </div>
+                    <div class="card-body p-0">
+                        @if (false)
+                        <ul class="">
+                           {{-- future work --}}
+                        </ul>
+                        @else
+                            <div class='alert alert-info m-2'><b>+ </b>Future Work</div>                 
+                        @endif
+                    </div>
+                    <!-- /.card-body -->
+                            
+                    <div class="card-footer text-center">
+                        <a href="#" title="Future work" class="uppercase">View All Satistics</a>
+                    </div>
+                    <!-- /.card-footer -->
+                </div>
+                <!-- /.card -->
+            </div>
+        </div>
+        
     </div>
     
 </div>
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -6,6 +6,8 @@
 
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
+    <link rel="icon" href="/logo.png" >
+
 
     <title>{{ config('app.name', 'DigiSay') }} @yield('title')</title>
 
@@ -25,69 +27,68 @@
 </head>
 <body>
     <div id="app">
-        <div id="topheader">
-            <nav class="navbar navbar-expand-md sticky-top navbar-dark bg-dark shadow-sm">
-                <div class="container">
-                    <a class="navbar-brand" href="{{ route('home') }}">
-                        {{ config('app.name', 'DigiSay') }}
-                    </a>
-                    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
-                        <span class="navbar-toggler-icon"></span>
-                    </button>
-    
-                    <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                        @auth
-                            <!-- Left Side Of Navbar -->
-                            <ul class="navbar-nav mr-auto">
-                                {{-- <li class="nav-item active">
-                                    <a class="nav-link" href="{{ route('home') }}">{{ __('Home') }}<span class="sr-only">(current)</span></a>
-                                </li> --}}
-                                <li class="nav-item">
-                                    <a class="nav-link" href="{{ route('clients.index') }}">{{ __('Clients') }}</a>
-                                </li>
-                                <li class="nav-item">
-                                    <a class="nav-link" href="{{ route('services.index') }}">{{ __('Services') }}</a>
-                                </li>
-                            </ul>
-                        @endauth
-                        
-    
-                        <!-- Right Side Of Navbar -->
-                        <ul class="navbar-nav ml-auto">
-                            <!-- Authentication Links -->
-                            @guest
-                                <li class="nav-item">
-                                    <a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a>
-                                </li>
-                                @if (Route::has('register'))
-                                    <li class="nav-item">
-                                        <a class="nav-link" href="{{ route('register') }}">{{ __('Register') }}</a>
-                                    </li>
-                                @endif
-                            @else
-                                <li class="nav-item dropdown">
-                                    <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
-                                        {{ Auth::user()->username }} <span class="caret"></span>
-                                    </a>
-    
-                                    <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
-                                        <a class="dropdown-item" href="{{ route('logout') }}"
-                                           onclick="event.preventDefault();
-                                                         document.getElementById('logout-form').submit();">
-                                            {{ __('Logout') }}
-                                        </a>
-    
-                                        <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
-                                            @csrf
-                                        </form>
-                                    </div>
-                                </li>
-                            @endguest
+        <nav class="navbar navbar-expand-md sticky-top navbar-dark bg-dark shadow-sm">
+            <div class="container">
+                <a class="navbar-brand" href="{{ route('home') }}">
+                    <img src="/logo.png" width="30" height="30" class="d-inline-block align-top" alt="">
+                    {{ config('app.name', 'DigiSay') }}
+                  </a>
+                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+
+                <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                    @auth
+                        <!-- Left Side Of Navbar -->
+                        <ul class="navbar-nav mr-auto">
+                            {{-- <li class="nav-item active">
+                                <a class="nav-link" href="{{ route('home') }}">{{ __('Home') }}<span class="sr-only">(current)</span></a>
+                            </li> --}}
+                            <li class="nav-item">
+                                <a class="nav-link" href="{{ route('clients.index') }}">{{ __('Clients') }}</a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link" href="{{ route('services.index') }}">{{ __('Services') }}</a>
+                            </li>
                         </ul>
-                    </div>
+                    @endauth
+                    
+
+                    <!-- Right Side Of Navbar -->
+                    <ul class="navbar-nav ml-auto">
+                        <!-- Authentication Links -->
+                        @guest
+                            <li class="nav-item">
+                                <a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a>
+                            </li>
+                            @if (Route::has('register'))
+                                <li class="nav-item">
+                                    <a class="nav-link" href="{{ route('register') }}">{{ __('Register') }}</a>
+                                </li>
+                            @endif
+                        @else
+                            <li class="nav-item dropdown">
+                                <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
+                                    {{ Auth::user()->username }} <span class="caret"></span>
+                                </a>
+
+                                <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
+                                    <a class="dropdown-item" href="{{ route('logout') }}"
+                                        onclick="event.preventDefault();
+                                                        document.getElementById('logout-form').submit();">
+                                        {{ __('Logout') }}
+                                    </a>
+
+                                    <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+                                        @csrf
+                                    </form>
+                                </div>
+                            </li>
+                        @endguest
+                    </ul>
                 </div>
-            </nav>
-        </div>
+            </div>
+        </nav>
         
 
         <main class="py-4">
@@ -97,7 +98,11 @@
         </main>
     </div>
 
-    <!-- Scripts -->
+    {{-- jQuery --}}
+    <script src="{{ asset('js/jquery-3.4.1.min.js') }}" defer></script>
+    {{-- <script src="http://ajax.aspnetcdn.com/ajax/jquery.validate/1.11.1/jquery.validate.min.js"></script> --}}
+    <script src="{{ asset('js/jquery.validate.min.js') }}" defer></script>
+    <!-- bootstrap Script -->
     <script src="{{ asset('js/app.js') }}" defer></script>
     {{-- Custom-js file --}}
     <script src="{{ asset('js/main.js') }}" defer></script>
